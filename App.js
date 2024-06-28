bf82ff21b8737aa0d09a896afae3816243cdd3e2
Iago Engel Serafim <iagoengel@yahoo.com>
2024-06-27 23:57:56 -0300

    Aula 3(Refatorando)

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import { SafeAreaView, StatusBar, View } from 'react-native';
 import { Montserrat_400Regular, Montserrat_700Bold, useFonts } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+
 import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta'
 
 export default function App() {
   const [fonteCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold
   })
-  console.log(fonteCarregada)
-  
+
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
-  
-  console.log(fonteCarregada)
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
